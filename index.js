e51eb349c60740dd66b77f0c7c0d46b2ab5ca2d2
Evan Anderson <40438869+TheeeEVan@users.noreply.github.com>
2022-05-25 19:34:35 +0000

    a

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const { PeerServer } = require('peer');
 // this function returns a string that will be used for ids
 const customGenerationFunction = () => (Math.random().toString(36)).substr(2, 5);
 
-const peerServer = PeerServer({ port: process.env.PORT || 9000, path: '/', generateClientId: customGenerationFunction });
+const peerServer = PeerServer({ port: 3000, path: '/queue', generateClientId: customGenerationFunction });
 
