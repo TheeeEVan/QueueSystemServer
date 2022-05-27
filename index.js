4fd7eeb2b1676f446e66f8e0808e1490c14e10e9
TheeeEVan <e.e.anderson@shaw.ca>
2022-05-27 16:25:16 -0600

    get ip and port

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const { PeerServer } = require('peer');
+import { PeerServer } from 'peer'
+import ip from 'public-ip';
+
+console.log(await ip.v4() + ":" + (process.env.PORT || 3000));
+
 
 // this function returns a string that will be used for ids
 const customGenerationFunction = () => (Math.random().toString(36)).substr(2, 5);
