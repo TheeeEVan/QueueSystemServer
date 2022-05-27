ae77d38668faf690b10880bd7a1c0e3df9de9591
TheeeEVan <e.e.anderson@shaw.ca>
2022-05-27 17:26:19 -0600

    discovery

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ import { PeerServer } from 'peer'
 // this function returns a string that will be used for ids
 const customGenerationFunction = () => (Math.random().toString()).substr(2, 5);
 
-const peerServer = PeerServer({ port: (process.env.PORT || 3000), generateClientId: customGenerationFunction });
+const peerServer = PeerServer({ port: (process.env.PORT || 3000), generateClientId: customGenerationFunction, allow_discovery: true });
 
