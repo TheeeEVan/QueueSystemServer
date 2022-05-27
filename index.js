292b0b490eb2ebac24e12503cf067310f26033a2
TheeeEVan <e.e.anderson@shaw.ca>
2022-05-27 17:10:36 -0600

    switch id to numbers only

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { PeerServer } from 'peer'
 
 
 // this function returns a string that will be used for ids
-const customGenerationFunction = () => (Math.random().toString(36)).substr(2, 5);
+const customGenerationFunction = () => (Math.random().toString()).substr(2, 5);
 
 const peerServer = PeerServer({ port: (process.env.PORT || 3000), generateClientId: customGenerationFunction });
 
