240d9a2cd8df72c99e0e49aff356f9e1503c534e
TheeeEVan <e.e.anderson@shaw.ca>
2022-05-27 17:01:35 -0600

    this should finally work

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import { PeerServer } from 'peer'
-import ip from 'public-ip';
-
-console.log(await ip.v4() + ":" + (process.env.PORT || 3000));
 
 
 // this function returns a string that will be used for ids
 const customGenerationFunction = () => (Math.random().toString(36)).substr(2, 5);
 
-const peerServer = PeerServer({ port: 3000, path: '/queue', generateClientId: customGenerationFunction });
+const peerServer = PeerServer({ port: (process.env.PORT || 3000), generateClientId: customGenerationFunction });
 
